d836bb05bb74cb0bf9ba0b0fc4125d2ad4bc704f
IsaiasZc <coroxerox2@gmail.com>
2022-05-18 23:52:11 -0500

    testimonials done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,18 @@ import './contact.css'
 
 function Contact() {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+        <article className="contact__option"></article>
+        </div>
+        {/* END OF CONTACT OPTIOS */}
+        <form action=""></form>
+      </div>
+    </section>
   )
 }
 
